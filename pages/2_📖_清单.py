fae0020894f38910e1c916c53b401f350e1c70d3
YikunHan <101811299+YikunHan42@users.noreply.github.com>
2023-03-07 20:48:33 +0800

    Update

--- pages/2_📖_清单.py
+++ pages/2_📖_清单.py
@@ -1,17 +1,17 @@
-import streamlit as st
-import base64
-# import os
-
-st.markdown("目前收录的妖怪清单如下：")
-
-file_path = './demon.pdf'
-
-with open(file_path,"rb") as f:
-    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-
-pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
-
-st.markdown(pdf_display, unsafe_allow_html=True)
+# import streamlit as st
+# import base64
+# # import os
+#
+# st.markdown("目前收录的妖怪清单如下：")
+#
+# file_path = './demon.pdf'
+#
+# with open(file_path,"rb") as f:
+#     base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+#
+# pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+#
+# st.markdown(pdf_display, unsafe_allow_html=True)
 
 
 # with open("post1-compressed.pdf", "rb") as pdf_file:
